172487d0603cfe6d648eed4ebbdc11af5e285d01
Emel Alkim <emelalkim@msn.com>
2017-10-17 12:48:58 -0700

    modified all the base64 encodings to use javautil class base64

--- src/main/java/edu/stanford/hakan/aim4api/database/exist/ExistManager.java
+++ src/main/java/edu/stanford/hakan/aim4api/database/exist/ExistManager.java
@@ -48,7 +48,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
-
+import java.util.Base64;
 /**
  *
  * @author localadmin
@@ -84,7 +84,7 @@ public class ExistManager {
                 ((HttpURLConnection) conn).setRequestProperty("Content-Type", "application/xml");
                 if (!"".equals(dbUserName.trim()) || !"".equals(dbUserPassword.trim())) {
                     String userPassword = dbUserName + ":" + dbUserPassword;
-                    String encoding = new sun.misc.BASE64Encoder().encode(userPassword.getBytes());
+                    String encoding = new String(Base64.getEncoder().encode(userPassword.getBytes()));
                     ((HttpURLConnection) conn).setRequestProperty("Authorization", "Basic " + encoding);
                 }
                 ((HttpURLConnection) conn).connect();
@@ -149,7 +149,7 @@ public class ExistManager {
                 ((HttpURLConnection) conn).setRequestProperty("Content-Type", "application/xml");
                 if (!"".equals(dbUserName.trim()) || !"".equals(dbUserPassword.trim())) {
                     String userPassword = dbUserName + ":" + dbUserPassword;
-                    String encoding = new sun.misc.BASE64Encoder().encode(userPassword.getBytes());
+                    String encoding =new String(Base64.getEncoder().encode(userPassword.getBytes()));
                     ((HttpURLConnection) conn).setRequestProperty("Authorization", "Basic " + encoding);
                 }
                 ((HttpURLConnection) conn).connect();
@@ -210,7 +210,7 @@ public class ExistManager {
                 ((HttpURLConnection) conn).setRequestProperty("Content-Type", "application/xml");
                 if (!"".equals(dbUserName.trim()) || !"".equals(dbUserPassword.trim())) {
                     String userPassword = dbUserName + ":" + dbUserPassword;
-                    String encoding = new sun.misc.BASE64Encoder().encode(userPassword.getBytes());
+                    String encoding =new String(Base64.getEncoder().encode(userPassword.getBytes()));
                     ((HttpURLConnection) conn).setRequestProperty("Authorization", "Basic " + encoding);
                 }
                 ((HttpURLConnection) conn).connect();
@@ -262,7 +262,7 @@ public class ExistManager {
                 ((HttpURLConnection) conn).setRequestProperty("Content-Type", "application/xml");
                 if (!"".equals(dbUserName.trim()) || !"".equals(dbUserPassword.trim())) {
                     String userPassword = dbUserName + ":" + dbUserPassword;
-                    String encoding = new sun.misc.BASE64Encoder().encode(userPassword.getBytes());
+                    String encoding = new String(Base64.getEncoder().encode(userPassword.getBytes()));
                     ((HttpURLConnection) conn).setRequestProperty("Authorization", "Basic " + encoding);
                 }
                 ((HttpURLConnection) conn).connect();
@@ -345,7 +345,7 @@ public class ExistManager {
                 ((HttpURLConnection) conn).setRequestProperty("Content-Type", "application/xml");
                 if (!"".equals(userName.trim()) || !"".equals(password.trim())) {
                     String userPassword = userName + ":" + password;
-                    String encoding = new sun.misc.BASE64Encoder().encode(userPassword.getBytes());
+                    String encoding = new String(Base64.getEncoder().encode(userPassword.getBytes()));
                     ((HttpURLConnection) conn).setRequestProperty("Authorization", "Basic " + encoding);
                 }
                 ((HttpURLConnection) conn).connect();
