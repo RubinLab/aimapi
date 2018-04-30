28177901fa260306da03101c86e57c554ff32f06
Emel Alkim <emelalkim@msn.com>
2018-04-30 15:11:42 -0700

    adds isline to aim class

--- src/main/java/edu/stanford/hakan/aim4api/project/epad/Aim4.java
+++ src/main/java/edu/stanford/hakan/aim4api/project/epad/Aim4.java
@@ -2141,6 +2141,10 @@ public class Aim4 extends ImageAnnotationCollection implements  Serializable {
 		return getShapeType(shapeID).equals(ShapeType.POLY);
 	}
 
+	public boolean isLine(int shapeID) {
+		return getShapeType(shapeID).equals(ShapeType.LINE);
+	}
+
 	
 	public Person createPerson(String name, String id, String sex,
             String birthdate) {
